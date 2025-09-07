a7d7e435f010bd284d7d41ada9e9056a0803c940
Subha833783 <99119889+Subha833783@users.noreply.github.com>
2025-09-08 05:07:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTQxNDM2NDUxNDU2ODM3NjU1MA.GwIlxs.482csspJ4LQ_3VchlJlng3ycjIieVh7YWg4DyQ",
   language: "en",
   ownerID: ["719531871494471701", ""], 
   mongodbUri : "mongodb+srv://subha833783_db_user:nDalioUuPOIDDbiz@cluster0.z7im8oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
