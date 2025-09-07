6e2e88d14914c11bdc9263b46ea3c7e8f3b39f68
Subha833783 <99119889+Subha833783@users.noreply.github.com>
2025-09-08 04:33:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://subha833783_db_user:nDalioUuPOIDDbiz@cluster0.z7im8oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
